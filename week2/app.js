d75ba42f1c713e759c1df4f257f539ea4b8f126f
Farhan Nazir <nazirf3440@gmail.com>
2018-10-15 16:24:11 +0200

    Homework Done

--- week2/app.js
+++ week2/app.js
@@ -10,6 +10,10 @@ function init() {
   vehicle("Blue", 2, 2);
   vehicleList("Pink", 0, 2);
   ad();
+  modules();
+  equalityTest();
+  objChange();
+  typeChecking();
 }
 
 //String  Challenges !
@@ -142,5 +146,65 @@ function ad() {
 }
 //11.
 list.push("Truck");
+
+//12.
+let emptyObj = {};
+console.log(emptyObj);
+
+//13 - 14
+function modules() {
+  let hyfModules = {
+    hyfTeacher: [
+      "Baraa",
+      "Tommy",
+      "Mike",
+      "Victor",
+      "Robin",
+      "Chanchal",
+      "Rasmus"
+    ]
+  };
+
+  console.log(hyfModules);
+
+  hyfModules.languages = ["html", "css", "javascript"];
+
+  console.log(hyfModules);
+}
+
+//. 15
+function equalityTest() {
+  let x = [1, 2, 3];
+  let y = [1, 2, 3];
+  let z = y;
+
+  console.log(x == y); // false
+  console.log(x === y); // false
+  console.log(z == y); // true
+  console.log(z == x); // false
+}
+
+//16
+function objChange() {
+  let o1 = { foo: "foo" };
+  let o2 = { foo: "bar" };
+  let o3 = o2;
+  let o4 = { foo: "bar" };
+  o3.bar = "foo";
+  console.log("o3 effect on o2 as well: ", JSON.stringify(o2));
+
+  o1 = { ...o4 };
+  o1.bar = "bar";
+  console.log(
+    "With this cloning method o1 never effect o4: " + JSON.stringify(o4)
+  );
+}
+
+//17
+function typeChecking() {
+  let bar = 42;
+  console.log(typeof bar);
+  //its a number.
+}
 
 init();
